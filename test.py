f5cde10721d8e4575c62598d9b14582de007cb6e
ghost22222248 <111342516+ghost22222248@users.noreply.github.com>
2023-05-15 11:06:24 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import time
+import requests
+import json
 def ma():
+  s='https://t-adder-app-clients-default-rtdb.firebaseio.com/.json'
+  i=1
   while(True):
-   print('hey')
+   requests.patch(s,json={'y':i})
+   i+=1
    time.sleep(2)
   
